d45db2c94d4063a7e86e1e8fe00c7eb26567e215
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-02-24 20:24:32 -0300

    scripts do exercício 1 finalizados

---
+++ exercises/INTRODUCTION/bloco_4/dia_1/adicao.js
@@ -0,0 +1,5 @@
+// programa de adição
+let a = 10;
+let b = 20;
+let soma = a + b;
+console.log(`A soma das variáveis a e b é: ${soma}`);
---
+++ exercises/INTRODUCTION/bloco_4/dia_1/divisao.js
@@ -0,0 +1,5 @@
+// programa de divisão
+let a = 10;
+let b = 20;
+let divisao = a / b;
+console.log(`A divisão das variáveis a e b é: ${divisao}`);
