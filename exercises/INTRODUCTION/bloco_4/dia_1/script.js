4df5234e57d05a40c9a9ecd83bcc6adca0e629ad
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-02-24 19:07:25 -0300

    add content script.js

---
+++ exercises/INTRODUCTION/bloco_4/dia_1/script.js
@@ -0,0 +1,7 @@
+const name = 'Anderson Nascimento';
+let birthCity = 'Maceió';
+const birthYear = 1991;
+let currentYear = 2021;
+console.log(`Meu nome é ${name}, nasci na cidade de ${birthCity} no ano de ${birthYear}, atualmente eu tenho ou vou completar ${currentYear-birthYear} anos.`);
+currentYear = "2022";
+console.log(`Meu nome é ${name}, nasci na cidade de ${birthCity} no ano de ${birthYear}, atualmente eu tenho ou vou completar ${currentYear-birthYear} anos.`);
