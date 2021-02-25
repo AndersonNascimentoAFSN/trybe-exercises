d8cd0e872b6d61e21ef539f2ca823433f4485ee7
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-02-24 21:00:10 -0300

    script da atividade 2 concluído

---
+++ exercises/INTRODUCTION/bloco_4/dia_1/maiornumero.js
@@ -0,0 +1,10 @@
+let number1 = 10;
+let number2 = 15;
+let maiorNumero = (number1>number2) ? number1 : number2;
+
+
+if(number1 === number2) {
+  console.log(`Os números ${number1} e ${number2} são iguais!`)
+} else {
+  console.log(`O maior número entre ${number1} e ${number2} é: ${maiorNumero}.`);
+}
