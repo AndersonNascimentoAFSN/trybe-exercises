adcae672ba3395fb48e5e901a726304f1e62bb65
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-02-25 16:18:52 -0300

    Exercícios de arrays movidos para o diretorio arrays

---
+++ exercises/INTRODUCTION/bloco_4/dia_2_0/arrays/contato.js
@@ -0,0 +1,7 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+// Forma alternativa: 
+// menu.splice(menu.length, 0, 'Contato')
+
+console.log(menu[menu.length - 1]);
+console.log(menu);
---
+++ exercises/INTRODUCTION/bloco_4/dia_2_0/arrays/servicos.js
@@ -0,0 +1,4 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServicesIndex = menu.indexOf('Serviços');
+let menuServices = menu[menuServicesIndex];
+console.log(menuServices);
