8fbf96c9289332de171d6040e635e59b549e1792
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-02-25 20:06:34 -0300

    exercicios for concluídos

---
+++ exercises/INTRODUCTION/bloco_4/dia_2_0/for/marcasCarros.js
@@ -0,0 +1,4 @@
+let cars = ['Saab', 'Volvo', 'BWM'];
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
---
+++ exercises/INTRODUCTION/bloco_4/dia_2_0/for/marcasCarrosFor.js
@@ -0,0 +1,6 @@
+let cars = ["Saab", "Volvo", "BMW"];
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i])
+}
+
+
