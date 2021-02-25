12149ccab5fa8c0a664774a1982cb1c33d6adc07
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-02-24 21:00:43 -0300

    script da atividade 3 concluído

---
+++ exercises/INTRODUCTION/bloco_4/dia_1/maiordetres.js
@@ -0,0 +1,11 @@
+let number1 = 10;
+let number2 = 15;
+let number3 = 25;
+
+if(number1 > number2 && number1 > number3){
+  console.log(`O número ${number1} é o maior entre os três`);
+} else if(number2 > number1 && number2 > number3){
+  console.log(`O número ${number2} é o maior entre os três`);
+} else {
+  console.log(`O número ${number3} é o maior entre os três`);
+}
