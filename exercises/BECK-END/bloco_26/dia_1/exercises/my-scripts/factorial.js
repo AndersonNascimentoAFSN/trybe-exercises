1f4e449a47489ca96a6e7983465b8f5935afd5c7
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-08-25 12:43:59 -0300

    fix: script node factorial

--- exercises/BECK-END/bloco_26/dia_1/exercises/my-scripts/factorial.js
+++ exercises/BECK-END/bloco_26/dia_1/exercises/my-scripts/factorial.js
@@ -1,6 +1,14 @@
-const { question } = require('readline-sync');
+const { questionInt } = require('readline-sync');
 
-const number = question("Digite um número para saber seu fatorial: ");
+const number = questionInt("Digite um número para saber seu fatorial: ");
+
+const factorial = (number) => {
+  if (number < 0) return false;
+  return number === 0 ? 1 : number * factorial(number - 1);
+}
+
+console.log(`O fatorial do número ${number} é ${factorial(number)}`);
+
 
 // const factorial = (number) => {
 //   // if (number < 0) return false;
@@ -9,11 +17,3 @@ const number = question("Digite um número para saber seu fatorial: ");
 // }
 
 // console.log(`O fatorial do número ${number} é ${factorial(number)}`); 
-
-
-const factorial = (number) => {
-  if (number < 0) return false;
-  return number === 0 ? 1 : number * factorial(number - 1);
-}
-
-console.log(`O fatorial do número ${number} é ${factorial(number)}`);
