5bb4c5726454746875c1ca0d9e465a79ad7e7dd6
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-29 16:36:06 -0300

    exercícios de operações básicas em Python

--- exercises/CIENCIA_DA_COMPUTACAO/BLOCO_32/dia_1/exercise_operacoes_basicas.py
+++ exercises/CIENCIA_DA_COMPUTACAO/BLOCO_32/dia_1/exercise_operacoes_basicas.py
@@ -1,10 +1,36 @@
 #  Exercício 1: No terminal, inicialize duas variáveis a e b, sendo a = 10 e b = 5. Mostre o resultado das 7 operações básicas (soma, subtração, multiplicação, divisão, divisão inteira, potenciação e módulo) envolvendo essas variáveis.
+a, b = 10, 5;
 
+print('Soma:', a + b);
+print('Subtração:', a - b);
+print('Multiplicação:', a * b);
+print('Divisão:', a / b);
+print('Divisão inteira:', a // b);
+print('Potenciação:', a ** b);
+print('Módulo:', a % b);
 
+# Exercício 2: Declare e inicialize uma variável: hours = 6. Quantos minutos têm em 6 horas? E quantos segundos? Declare e inicialize variáveis minutes e seconds que recebem os respectivos resultados das contas. Depois, imprima cada uma delas.
+hours = 6;
+minutesPerHours = 60;
+secondsPerHours = 60 * 60;
 
-# Exercício 2: Declare e inicialize uma variável: hours = 6 . Quantos minutos têm em 6 horas? E quantos segundos? Declare e inicialize variáveis minutes e seconds que recebem os respectivos resultados das contas. Depois, imprima cada uma delas.
+minutes = hours * minutesPerHours;
+seconds = hours * secondsPerHours;
+
+print("Minutos em 6 horas:", minutes);
+print("Segundos em 6 horas:", seconds);
+
 # Exercício 3: Teste e verifique o que acontece se você colocar um ponto e vírgula no final de uma instrução em Python.
+# R = Não acontece nada;
 
 
+# Exercício 4: Suponha que o preço de capa de um livro seja 24,20, mas as livrarias recebem um desconto de 40%. O transporte custa 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. Qual é o custo total de atacado para 60 cópias? Escreva uma expressão que receba o custo total e a imprima.
+totalBookUnit = 24.20;
+qtyOfBooks = 60;
+firstTransport = 3.00;
+transport = 0.75;
+taxaDescont = 0.4;
+totalDescont = (totalBookUnit * qtyOfBooks * taxaDescont)
 
-# Exercício 4: Suponha que o preço de capa de um livro seja 24,20, mas as livrarias recebem um desconto de 40%. O transporte custa 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. Qual é o custo total de atacado para 60 cópias? Escreva uma expressão que receba o custo total e a imprima. 
+totalCost = (totalBookUnit * qtyOfBooks) + firstTransport + (qtyOfBooks - 1 * transport) - totalDescont;
+print("Total cost:", totalCost);
