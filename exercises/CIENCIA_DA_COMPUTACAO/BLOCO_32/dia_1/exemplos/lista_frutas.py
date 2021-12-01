2e77a3b4503ac8e171499859b842c4ea24aa8671
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-01 02:09:42 -0300

    resolve format

--- exercises/CIENCIA_DA_COMPUTACAO/BLOCO_32/dia_1/exemplos/lista_frutas.py
+++ exercises/CIENCIA_DA_COMPUTACAO/BLOCO_32/dia_1/exemplos/lista_frutas.py
@@ -1,18 +1,19 @@
-# 1) Crie uma variável do tipo lista chamada lista_frutas contendo os valores "banana",
+# 1) Crie uma variável do tipo lista chamada lista_frutas contendo os valores
+# "banana",
 # "uva", "maça" e "pera"
 
-lista_frutas = ["banana", "uva", "maça", "pera"];
+lista_frutas = ["banana", "uva", "maça", "pera"]
 
 # 2) Usando um loop for: Para cada fruta na lista, imprima a 1a letra da fruta
 
 for fruta in lista_frutas:
-  print('Primeira letra da fruta:', fruta[0]);
+    print("Primeira letra da fruta:", fruta[0])
 
-# 3) Usando um loop for: Para cada fruta na lista, imprima a última letra da fruta
+# 3) Usando um loop for: Para cada fruta na lista, imprima a última letra da
+# fruta
 for fruta in lista_frutas:
-  print('Última letra da fruta:', fruta[-1]);
+    print("Última letra da fruta:", fruta[-1])
 
 # 4) Usando um loop for: Para cada fruta na lista, imprima a 4a letra da fruta
 # for fruta in lista_frutas:
 #   print('Quarta letra da fruta:', fruta[3]);
-
